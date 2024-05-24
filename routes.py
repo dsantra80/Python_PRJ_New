1c2dd14bab8ba0c33fdc4ae8b3c6a4c151768f97
dsantra80 <60920557+dsantra80@users.noreply.github.com>
2024-05-24 16:08:54 +0530

    Updated routes.py

--- routes.py
+++ routes.py
@@ -16,7 +16,7 @@ if not huggingface_token:
 tokenizer = AutoTokenizer.from_pretrained(model_id, use_auth_token=huggingface_token)
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
-    torch_dtype=torch.bfloat16,
+   # torch_dtype=torch.bfloat16,
     device_map="auto",
     use_auth_token=huggingface_token
 )
