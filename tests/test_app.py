9b761851db4935373df39d97c1d4f817d1f58931
dsantra80 <60920557+dsantra80@users.noreply.github.com>
2024-05-24 13:40:33 +0530

    Updated test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,10 +2,6 @@ import sys
 import os
 import pytest
 from flask import Flask
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
 
 # Ensure the application root is in the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
